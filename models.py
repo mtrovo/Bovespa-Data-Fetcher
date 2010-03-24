33d9963d7a12278a83cf19b288a4f8c1e9e257ab
Moises Trovo <mtrovo@mtrovo-laptop.(none)>
2010-03-24 01:55:54 -0300

    Stock model

--- models.py
+++ models.py
@@ -9,4 +9,15 @@ class Company(db.Model):
 
     def __str__(self):
         return "<Company code: %s, idt: %s, name: %s>" % (repr(self.code), repr(self.idt), repr(self.name))
-        
+        class Stock(db.Model):
+   company = db.ReferenceProperty(Company)
+   date = db.DateTimeProperty()
+   price = db.FloatProperty()
+   low = db.FloatProperty()
+   high = db.FloatProperty()
+   var = db.FloatProperty()
+   varpct = db.FloatProperty()
+   vol = db.FloatProperty()
+   
+   def __str__(self):
+      return "<Stock code: " % ()
